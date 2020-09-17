c9f1241fad3c86dca72fd3923dc881b7724a48d4
cosmosno <cosmosno@snu.ac.kr>
2020-09-17 22:04:10 +0900

    lab session #3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TodoList from './containers/TodoList/TodoList';
+import NewTodo from './containers/TodoList/NewTodo/NewTodo';
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
+import TodoDetail from './components/TodoDetail/TodoDetail';
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
+      <Switch>
+        <Route path="/todos" exact render={() => <TodoList title={"My TODOs"}></TodoList>}></Route>
+        <Route path="/new-todo" exact component={NewTodo}></Route>
+        <Redirect exact from='/' to='todos'></Redirect>
+        <Route render={() => <h1>Not Found</h1>}></Route>
+      </Switch>
     </div>
+    </BrowserRouter>
   );
 }
 
